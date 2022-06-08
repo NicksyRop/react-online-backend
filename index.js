3ddbb6aa7ef0794718316b7effb9cf4497f175b2
NicksyRop <nicksonkipkorir25@gmail.com>
2022-06-08 14:14:59 +0300

    created a products array

--- index.js
+++ index.js
@@ -7,3 +7,15 @@ const app = express();
 app.use(express.json());
 
 app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to our online shop API");
+});
+
+app.get("/products", (req, res) => {
+  res.send([1, 2, 3]);
+});
+
+const Port = process.env.PORT || 5000;
+
+app.listen(Port, console.log(`Server running on port ${Port}`));
---
+++ products.js
@@ -0,0 +1,32 @@
+const Products = [
+  {
+    id: 1,
+    name: "Iphone 12 pro",
+    desc: "6.1 inch display",
+    price: 999,
+    image: "",
+  },
+  {
+    id: 2,
+    name: "Redmi note 11",
+    desc: "90 hz refresh inch display",
+    price: 750,
+    image: "",
+  },
+  {
+    id: 3,
+    name: "Samsung A12",
+    desc: "Lcd panel display",
+    price: 820,
+    image: "",
+  },
+  {
+    id: 4,
+    name: "Huawei nova",
+    desc: "6.29 amoloed display",
+    price: 605,
+    image: "",
+  },
+];
+
+module.exports = Products;
